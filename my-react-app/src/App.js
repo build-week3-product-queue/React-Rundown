ee9842d241142eb06c252ca144c4d6df9d4e10cd
jarred richards <jarred2828@gmail.com>
2019-05-01 13:10:37 -0400

    ComponentsAdded

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
+import profileForm from './components/profileForm';
+import profiles from './components/profiles';
+import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurfs: [],
+    };
+  }
+  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  // Notice what your map function is looping over and returning inside of Smurfs.
+  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    const endpoint = 'http://localhost:3333/smurfs';
+
+    axios
+      .get(endpoint)
+      .then(response => {
+        this.setState({smurfs: response.data });
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      })
+  }
+
+  newSmurf = (data) => {
+    axios
+    .post('http://localhost:3333/smurfs', data)
+    .then(response => {
+      this.setState({
+        profiles: response.data
+      })
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <div className="nav-links">
+            <NavLink className="nav-link"  to='/profile-form'>
+              Add profile
+            </NavLink>
+            <NavLink className="nav-link" exact to='/'>
+              profiles
+            </NavLink>
+          </div>
+        </nav>
+        <Route path='/profile-form' render={props => <profileForm
+        {...props}
+        newProfile={this.newProfile}
+        />} />
+        <Route  exact path='/' render={props => <profiles 
+        {...props}
+        profiles={this.state.profiles} />} />
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
